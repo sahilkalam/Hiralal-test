ec0309fa38642efc2fa5011fd5ede2e6a360182b
Sahil Kalam <sahilkalam000@gmail.com>
2026-06-27 08:37:50 +0530

    error fix

--- public/firebase/firebaseClient.js
+++ public/firebase/firebaseClient.js
@@ -1,16 +1,17 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDWkupqoh_LpZAGZsYYJalDYmnrkcL8L80",
   authDomain: "hiralal-app.firebaseapp.com",
   projectId: "hiralal-app",
-  storageBucket: "hiralal-app.firebasestorage.app",
+  storageBucket: "hiralal-app.appspot.com",
   messagingSenderId: "1086744796561",
   appId: "1:1086744796561:web:8b4d6653010a741b8c8e30",
   measurementId: "G-80JDVJXGEX"
 };
 
-// Next.js SSR error se bachne ke liye condition check
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// Safe init (Next.js SSR safe)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-export { app };
+export default app;
